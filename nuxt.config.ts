02908b1885f68e1bf885cd5994efcc2b89930f76
fabkho <fab.kirchhoff98@gmail.com>
2026-05-16 21:19:11 +0200

    debug: enable VUE_PROD_HYDRATION_MISMATCH_DETAILS to trace blog hydration error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,7 +69,10 @@ export default defineNuxtConfig({
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    define: {
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+    }
   },
 
   eslint: {
